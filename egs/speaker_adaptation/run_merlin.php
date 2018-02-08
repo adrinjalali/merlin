6965109f826f3ec78dab1d6880918226b1d5b691
Athayde, Mariana <mariana.athayde@ancud.de>
2018-02-08 10:11:09 +0100

    conversion of a single text returns the corresponding .wav

--- egs/speaker_adaptation/run_merlin.php
+++ egs/speaker_adaptation/run_merlin.php
@@ -3,6 +3,18 @@
 $text = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $text = $_POST["text"];
-  $output = `s1/generate-voice-from-text.sh "{$text}" slt_arctic`;
+  $text     = $_POST["text"];
+  $filename = substr($text, 0, 40);
+  $filename = str_replace(" ", "_", $filename);
+  $filename = preg_replace('/[^A-Za-z0-9\_]/', '', $filename);
+  `s1/generate-voice-from-text.sh "{$text}" "{$filename}" slt_arctic`;
+}else{
+  print("what do you think you're doing??");
 }
+?>
+<head>
+  HERE IS YOUR FILE
+</head>
+<body>
+  <a href="/s1/experiments/slt_arctic/test_synthesis/wav/<?php echo $filename?>.wav" download>Click to download</a>
+</body>
--- egs/speaker_adaptation/s1/generate-voice-from-text.sh
+++ egs/speaker_adaptation/s1/generate-voice-from-text.sh
@@ -5,28 +5,26 @@ cd s1
 global_config_file=conf/global_settings.cfg
 source $global_config_file
 
-if test "$#" -ne 2; then
-    echo
-    echo "################################"
-    echo "Usage: "
-    echo "./generate-voice-from-text.sh <text_to_synthesize> <previously_configured_voice_name>"
-    echo "################################"
+if test "$#" -ne 3; then
+    #echo "Usage: "
+    #echo "./generate-voice-from-text.sh <text_to_synthesize> <previously_configured_voice_name>"
     exit 1
 fi
 
 in_txt=$1
-filename=${in_txt// /_} #remove white spaces
-filename=`echo $filename| tr -dc '[:alnum:]\n\r'`
-text_dir=experiments/$2/test_synthesis/custom_txt
+filename=$2 #remove white spaces
+text_dir=experiments/$3/test_synthesis/custom_txt
 
 touch $text_dir/$filename.txt
 echo $in_txt > $text_dir/$filename.txt
 
-test_dur_conf_file=conf/test_dur_synth_$2.conf
-test_synth_conf_file=conf/test_synth_$2.conf
+test_dur_conf_file=conf/test_dur_synth_$3.conf
+test_synth_conf_file=conf/test_synth_$3.conf
 
 lab_dir=$(dirname $text_dir)
 ./scripts/prepare_labels_from_txt.sh $text_dir $lab_dir $global_config_file
 ./scripts/submit.sh ${MerlinDir}/src/run_merlin.py $test_dur_conf_file
 ./scripts/submit.sh ${MerlinDir}/src/run_merlin.py $test_synth_conf_file
 ./scripts/remove_intermediate_files.sh $global_config_file
+
+echo $filename
